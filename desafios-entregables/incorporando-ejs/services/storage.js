dbdffa62bc451a7ca094bb2157d99c5430bc60d1
Fullstack Developer <mauricio.suarez25@gmail.com>
2022-03-19 09:10:17 -0400

    Added pug and ejs folders

---
+++ desafios-entregables/incorporando-ejs/services/storage.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const FILE_TYPE_MAP = {
+  // mime type
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads');
+  },
+  filename: (req, file, cb) => {
+    const filename = file.originalname;
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, filename);
+  },
+});
+
+module.exports = storage;
